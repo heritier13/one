bd3474244b3ad469672f34e31bfeff72035b38f2
heritier13 <166453523+heritier13@users.noreply.github.com>
2024-12-11 03:30:56 -0800

    Update index.jsz

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-  import initializeApp  from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  import initializeApp from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyBd1kXVO9VeXqFVXxERnCUyEKHBlrwwRzA",
