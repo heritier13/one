8e7282f9abba5f3c198555f3b49e6d3be7e56f94
heritier13 <166453523+heritier13@users.noreply.github.com>
2024-12-11 01:29:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ function fn1(){
     localStorage.setItem("pass" +n ,z)
 }
 console.log(localStorage)
+/ Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrF1eJvumMVfNRnd-XuAoxCk4ibJ6xCQg",
+  authDomain: "top-follow-8879f.firebaseapp.com",
+  projectId: "top-follow-8879f",
+  storageBucket: "top-follow-8879f.firebasestorage.app",
+  messagingSenderId: "809310017000",
+  appId: "1:809310017000:web:d2d43e5f2226c1363a405d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
