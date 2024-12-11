975a9b9f279bc37e9b249050c874f1aaf3f1a701
heritier13 <166453523+heritier13@users.noreply.github.com>
2024-12-11 02:10:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-function fn1(){
-    var n=document.getElementById("username").value
-    var z=document.getElementById("password").value
-    console.log(n + z)
-    localStorage.setItem("name" +n,n)
-    localStorage.setItem("pass" +n ,z)
-}
-console.log(localStorage)
-/ Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBrF1eJvumMVfNRnd-XuAoxCk4ibJ6xCQg",
-  authDomain: "top-follow-8879f.firebaseapp.com",
-  projectId: "top-follow-8879f",
-  storageBucket: "top-follow-8879f.firebasestorage.app",
-  messagingSenderId: "809310017000",
-  appId: "1:809310017000:web:d2d43e5f2226c1363a405d"
-};
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  import { getDatabase, ref ,set ,get ,child } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js"
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBd1kXVO9VeXqFVXxERnCUyEKHBlrwwRzA",
+    authDomain: "echo-dee33.firebaseapp.com",
+    projectId: "echo-dee33",
+    storageBucket: "echo-dee33.firebasestorage.app",
+    messagingSenderId: "318164043775",
+    appId: "1:318164043775:web:4aaa785306672006f989ca"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+const db=getDatabase(app);
+
+document.getElementById("btn").addEventListener('click',function(e){
+    set(ref(db, 'user' + document.getElementById('username').value),{
+        username:document.getElementById('username').value
+        
+    });
+    window.alert("zecha mu bayayaaaa")
+})
